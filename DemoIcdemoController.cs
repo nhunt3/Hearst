c1dc8ea9a167f81c6849dc66f500585721252d78
Nick Hunt <nhunt11@gmail.com>
2016-11-02 20:58:39 -0400

    my commit

---
+++ DemoIcdemoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using System.Data;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WebApplication1
+{
+    public class DemoIcdemoController : ApiController
+    {
+        [System.Web.Http.AcceptVerbs("GET", "POST")]
+        [System.Web.Http.HttpPost]
+        public string GetData()
+        {
+            return File.ReadAllText(@"C:\Users\nhunt\Desktop\makes.txt").Replace("\t", "").Replace("\r\n", "");
+        }
+
+    }
+}
---
+++ demo-icdemo.js
@@ -0,0 +1,42 @@
+﻿import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
+
+@inject(HttpClient)
+export class DemoIcdemo 
+{
+    constructor(http)
+    {
+        http.configure(config => {
+            config
+              .withBaseUrl('api/')
+              .withDefaults({
+                  headers: {
+                      'Accept': 'application/json'
+                  }
+              });
+        });
+
+        this.http = http;       
+    }
+    
+    activate()
+    {
+        this.http.fetch('DemoIcdemo/GetData').then(response => response.json()).then(info => 
+        { 
+            let allInfo = JSON.parse(info);
+            this.makes = allInfo.makes;
+            this.models = allInfo.models;
+            this.cars = allInfo.cars;
+        });
+    }
+
+    showImage(car_image, car_text, model_name)
+    {
+        car_text.style.display = '';
+        car_text.innerHTML = model_name;
+
+        car_image.style.display = '';
+        car_image.src = "src/Pages_Overview/" + model_name + ".jpg";
+    }
+}
